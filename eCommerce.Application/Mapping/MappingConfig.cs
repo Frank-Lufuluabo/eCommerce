6b19cd05885935c14b84d3ed6c39944b39e7870a
Frank Kalengay <frankkalengay@gmail.com>
2026-09-04 00:22:11 +0200

    feat: update application mapping configuration

--- eCommerce.Application/Mapping/MappingConfig.cs
+++ eCommerce.Application/Mapping/MappingConfig.cs
@@ -1,7 +1,9 @@
 ﻿using AutoMapper;
 using eCommerce.Application.DTOs.Category;
+using eCommerce.Application.DTOs.Identity;
 using eCommerce.Application.DTOs.Product;
 using eCommerce.Domain.Entities;
+using eCommerce.Domain.Entities.Identity;
 
 
 namespace eCommerce.Application.Mapping
@@ -15,6 +17,9 @@ namespace eCommerce.Application.Mapping
 
             CreateMap<Category, GetCategory>();
             CreateMap<Product, GetProduct>();
+
+            CreateMap<CreateUser, AppUser>();
+            CreateMap<LoginUser, AppUser>();
         }
     }
 }
