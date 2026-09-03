53f6609c8dd94aa07a4c8fc5502e06d199967e49
Frank Kalengay <frankkalengay@gmail.com>
2026-09-04 00:22:31 +0200

    feat: update application database context

--- eCommerce.Infrastructure/Data/AppDbContext.cs
+++ eCommerce.Infrastructure/Data/AppDbContext.cs
@@ -1,5 +1,6 @@
 ﻿using eCommerce.Domain.Entities;
 using eCommerce.Domain.Entities.Identity;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
@@ -15,6 +16,26 @@ namespace eCommerce.Infrastructure.Data
         public DbSet<Product> Products { get; set; }
         public DbSet<Category> Categories { get; set; }
         public DbSet<RefreshToken> RefreshToken { get; set; }
-    }
 
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            builder.Entity<IdentityRole>()
+                .HasData(
+                new IdentityRole
+                {
+                    Id = Guid.NewGuid().ToString(),
+                    Name = "Admin",
+                    NormalizedName = "ADMIN"
+                },
+                new IdentityRole
+                {
+                    Id = Guid.NewGuid().ToString(),
+                    Name = "User",
+                    NormalizedName = "USER"
+
+                });
+        }
+    }
 }
